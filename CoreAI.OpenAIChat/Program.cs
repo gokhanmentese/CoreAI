a85093bb7a9e54dfb4f44f6bd80ef6f1f76ebc26
Gökhan Menteşe <gkhnmnts@gmail.com>
2025-04-30 03:42:58 +0300

    Add project files.

---
+++ CoreAI.OpenAIChat/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text;
+
+var apiKey = "open-ai-secret-key";
+
+using var client = new HttpClient();
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+var requestData = new
+{
+    model = "gpt-3.5-turbo",
+    messages = new[] {
+        new {role = "system", content = "You are a helpful assistant"},
+        new {role = "user", content = "Merhaba, nasılsın?"}
+    },
+    max_tokens = 100 // uygulamanın bize getirdiği içeriğin uzunlugu
+};
+
+var json = JsonSerializer.Serialize(requestData);
+var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+var response = await client.PostAsync("https://api.openai.com/v1/chat/completions", content);
+var responseBody = await response.Content.ReadAsStringAsync();
+
+if (response.IsSuccessStatusCode)
+{
+    var result = JsonSerializer.Deserialize<JsonElement>(responseBody);
+    var answer = result.GetProperty("choices")[0].GetProperty("message").GetProperty("content").GetString();
+
+    Console.WriteLine($"Open AI'ın cevabı:{answer}");
+}
+else
+{
+    Console.WriteLine("Bir hata olustu");
+}
+
+Console.WriteLine(responseBody);
---
+++ CoreAI.SpeechToText/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text;
+
+var apiKey = "open-ai-secret-key";
+var audioFilePath = "audio.mp3";
+
+using var client = new HttpClient();
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+var form = new MultipartFormDataContent();
+
+var audioContent = new ByteArrayContent(File.ReadAllBytes(audioFilePath));
+audioContent.Headers.ContentType = MediaTypeHeaderValue.Parse("audio/mpeg");
+form.Add(audioContent,"file",Path.GetFileName(audioFilePath));
+form.Add(new StringContent("whisper-1"),"model");
+
+Console.WriteLine("Ses dosyası işleniyor, lütfen bekleyiniz");
+
+var response = await client.PostAsync("https://api.openai.com/v1/audio/transcriptions", form);
+
+if (response.IsSuccessStatusCode)
+{
+    var responseBody = await response.Content.ReadAsStringAsync();
+
+
+    Console.WriteLine($"Transkript");
+}
+else
+{
+    Console.WriteLine("Bir hata olustu"); 
+}
+
+
